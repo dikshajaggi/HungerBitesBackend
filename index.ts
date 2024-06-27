28633eb9fa7e3f5480f57fd93afae7891c42099a
cee-diksha <cee.diksha@gmail.com>
2024-06-27 16:20:49 +0530

    modifications done

--- index.ts
+++ index.ts
@@ -18,7 +18,7 @@ app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 app.use(express.json());
 app.use(bodyParser.json());
-app.use(cors({ origin: 'http://localhost:3001' }))
+app.use(cors({ origin: 'http://localhost:3000' }))
 app.get("/health", async (req: Request, res: Response) => {
   res.send({ message: "health OK!" });
 });
